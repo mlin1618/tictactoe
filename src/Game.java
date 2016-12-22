b3924f83f27b307dc3456f2b1a7d7dccbfd60daa
mlin1618 <maxlin1234@gmail.com>
2016-12-21 20:44:04 -0500

    ..

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,7 @@
  */
 public class Game {
     public int[][] board = new int[3][3];
-    public int[][] meta = new int[8][];
+    public int[][] meta = new int[8][];//meta: 0,1,2 3 rows, 3,4,5, 3 columns, 6, 7 2 diagonals
     public int moves = 0;
     public int type;
     public Game(int n){
@@ -24,17 +24,91 @@ public class Game {
         }
         return true;
     }
-    //checkMeta: {a,b} - a is 0 or 1 (win or not), b is which player
-    public int[] checkMeta(){
+    //checkMeta: if N=0, check for win, if N=1 check for 2 consecutive
+    //returns array {a,b,c,d} a is win or not, b is which player winning or almost win, c is column/diagonal/row, d is which one
+    public int checkWin(){
         for(int i = 0; i < 8; i++){
             for(int j = 0; j < 2; j++){
                 if(meta[i][j] == 3){
-                    // return {0, j};
+                    return (j==0)?1:-1;
                 }
             }
         }
+        return 0;
     }
-    public int[][] AI(){
+    public int checkTwo(){
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 2; j++){
+                if(meta[i][j] == 2 && meta[i][(j+1)%2] == 0){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    public void AI(boolean c){
+        int t = checkTwo();
+        if(t!=-1){
+            if(t <= 2){
+                for(int i = 0; i < 3; i++){
+                    if(board[t][i] ==0){
+                        move(i,t,c);
+                    }
+                }
+            }
+            else if(t <= 5){
+                for(int i = 0; i < 3; i++){
+                    if(board[i][t] ==0){
+                        move(t,i,c);
+                    }
+                }
+            }
+            else if(t==6){
+                for(int i = 0; i < 3; i++){
+                    if(board[i][i] ==0){
+                        move(i,i,c);
+                    }
+                }
+            }
+            else{
+                for(int i = 0; i < 3; i++){
+                    if(board[i][2-i] ==0){
+                        move(2-i,i,c);
+                    }
+                }
+            }
+        }
+        else{
+            if(board[1][1] == 0){
+                move(1,1,c);
+            }
+            else{
+                boolean f = false;
+                for(int i = 0; i < 2; i++){
+                    for(int j = 0; j < 2; j++){
+                        if(board[2*i][2*j] ==0 ){//add something to check for forks
+                            move(2*j, 2*i, c);
+                            f = !f;
+                            break;
+                        }
+                    }
+                }
+                if(!f){
+                    if(board[1][0] == 0){
+                        move(0,1,c);
+                    }
+                    else if(board[0][1] ==0){
+                        move(1,0,c);
+                    }
+                    else if(board[2][1] ==0){
+                        move(1,2,c);
+                    }
+                    else{
+                        move(2,1,c);
+                    }
+                }
+            }
+        }
 
     }
     public void printBoard(){
